a07379139fd545d8ba2bea7c05812f261bc2a9d1
tuna510 <56784079+tuna510@users.noreply.github.com>
2019-11-03 20:42:30 +0900

    Update file1.js

--- js/file1.js
+++ js/file1.js
@@ -1,69 +1,3 @@
-// ヘッダー
-
-function header_dayo() {
-    var html = "";
-    html += '<header id="header">';
-    html += '<nav>';
-    html += '<ul>';
-    html += '<font size="2">';
-    html += '<li class=""><a href="../index.html">一覧</a></li>';
-    html += '<li>';
-    html += '<div class="content">';
-    html += '<a class="modal-new-open" href="">更新履歴</a>';
-    html += '</div>';
-    html += '<div class="modal modal-new">';
-    html += '<div class="modal_n_i modal-new-close"></div>';
-    html += '<div class="modal_n_c">';
-    html += '<p>2019.11.1 　とりあえず公開</p>';
-    html += '<a class="modal-new-close" href="">閉じる</a>';
-    html += '</div>';
-    html += '</div>';
-    html += '</li>';
-    html += '<li>';
-    html += '<div class="content">';
-    html += '<a class="modal-here-open" href=""> ここ何？</a>';
-    html += '</div>';
-    html += '<div class="modal modal-here">';
-    html += '<div class="modal_h_i modal-here-close"></div>';
-    html += '<div class="modal_h_c">';
-    html += '<p id="modal-h-text">後藤の後藤による後藤のための備忘録<br><br>';
-    html += 'ターゲット：後藤　目的：脳みそのUSB(?)ほしい<br>';
-    html += 'クレーム云々は受け付けてないけど<br>';
-    html += 'ちゃまは何かあったら教えてね♡<br>';
-    html += '他の方も何かあればフォームからどうぞ<br>';
-    html += 'スマホ対応はしていません<br>';
-    html += 'ラーメン食べたい</p>';
-    html += '<a class="modal-here-close" href="">閉じる</a>';
-    html += '</div>';
-    html += '</div>';
-    html += '</li>';
-    html += '<li>';
-    html += '<div class="content">';
-    html += '<a class="modal-form-open" href="#">フォーム</a>';
-    html += '</div>';
-    html += '<div class="modal modal-form">';
-    html += '<div class="modal_f_i modal-form-close"></div>';
-    html += '<div class="modal_f_c">';
-    html += '<p>作ってはみたけどいらなくね？直接言ってくれ<br>';
-    html += 'なんもないのでタスクリスト<br>';
-    html += '・コードに色つける<br>';
-    html += '・flaskとかのコード欄 言語で背景色変える？コードに色つけてから<br>';
-    html += '・途中で挟む画像の幅大きくする？<br>';
-    html += '・ボタンの形変更<br>';
-    html += '・下段(JS5とか)のページは内容書くときに整えます<br>';
-    html += '・内容自体はぼちぼち更新<br></p>';
-    html += '<a class="modal-form-close" href="">閉じる</a>';
-    html += '</div>';
-    html += '</div>';
-    html += '</li>';
-    html += '</font>';
-    html += '</ul>';
-    html += '</nav>';
-    html += '</header>';
-    document.write(html);
-}
-
-// --------------------------------------------
 // ぽよん
 $(function () {
     $('.modal-new-open').on('click', function () {
@@ -127,4 +61,73 @@ $(function () {
     });
 });
 
+
+
 // --------------------------------------
+
+function header_dayo() {
+    var html = "";
+    html += '<header id="header">';
+    html += '<nav>';
+    html += '<ul>';
+    html += '<font size="2">';
+    html += '<li class="current"><a href="">一覧</a></li>';
+    html += '<li>';
+    html += '<div class="content">';
+    html += '<a class="modal-new-open" href="">更新履歴</a>';
+    html += '</div>';
+    html += '<div class="modal modal-new">';
+    html += '<div class="modal_n_i modal-new-close"></div>';
+    html += '<div class="modal_n_c">';
+    html += '<p>2019.11.3 　htm&css、js5#jquery更新</p>';
+    html += '<p>2019.11.1 　とりあえず公開</p>';
+    html += '<a class="modal-new-close" href="">閉じる</a>';
+    html += '</div>';
+    html += '</div>';
+    html += '</li>';
+    html += '<li>';
+    html += '<div class="content">';
+    html += '<a class="modal-here-open" href=""> ここ何？</a>';
+    html += '</div>';
+    html += '<div class="modal modal-here">';
+    html += '<div class="modal_h_i modal-here-close"></div>';
+    html += '<div class="modal_h_c">';
+    html += '<p id="modal-h-text">後藤の後藤による後藤のための備忘録<br><br>';
+    html += 'ターゲット：後藤　目的：脳みそのUSB(?)ほしい<br>';
+    html += 'クレーム云々は受け付けてないけど<br>';
+    html += 'ちゃまは何かあったら教えてね♡<br>';
+    html += '他の方も何かあればフォームからどうぞ<br>';
+    html += 'スマホ対応はしていません<br>';
+    html += 'ラーメン食べたい</p>';
+    html += '<a class="modal-here-close" href="">閉じる</a>';
+    html += '</div>';
+    html += '</div>';
+    html += '</li>';
+    html += '<li>';
+    html += '<div class="content">';
+    html += '<a class="modal-form-open" href="#">フォーム</a>';
+    html += '</div>';
+    html += '<div class="modal modal-form">';
+    html += '<div class="modal_f_i modal-form-close"></div>';
+    html += '<div class="modal_f_c">';
+    html += '<pre>作ってはみたけどいらなくね？直接言ってくれ';
+    html += 'なんもないのでタスクリスト';
+    html += '・コードに色つける';
+    html += '・flaskとかhtml&cssとかのコード欄 言語で背景の色変える？コードに色つけてから検討';
+    html += '・途中で挟む画像の幅大きくする？';
+    html += '・ボタンの形変更';
+    html += '・top以外のヘッダーの内容統一するよ';
+    html += '・下段(JS5とか)のページは内容書くときに整えます';
+    html += '・内容自体はぼちぼち更新</pre>';
+    html += '<a class="modal-form-close" href="">閉じる</a>';
+    html += '</div>';
+    html += '</div>';
+    html += '</li>';
+    html += '</font>';
+    html += '</ul>';
+    html += '</nav>';
+    html += '</header>';
+    document.write(html);
+}
+
+// html += '';
