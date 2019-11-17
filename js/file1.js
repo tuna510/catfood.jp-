0625fde8ee6b3e5d6fdf7582a6c125d1831bcdc3
tuna510 <56784079+tuna510@users.noreply.github.com>
2019-11-17 10:16:54 +0900

    Update file1.js

--- js/file1.js
+++ js/file1.js
@@ -79,8 +79,9 @@ function header_dayo() {
     html += '<div class="modal modal-new">';
     html += '<div class="modal_n_i modal-new-close"></div>';
     html += '<div class="modal_n_c">';
+    html += '<p>2019.11.16　TOPにReactを追加 Reactに内容追加</p>';
     html += '<p>2019.11.15　PHPの内容を追加</p>';
-    html += '<p>2019.11.14 　TOPにjavaとPHPを追加 内容はjavaのみ</p>';
+    // html += '<p>2019.11.14 　TOPにjavaとPHPを追加 内容はjavaのみ</p>';
     // html += '<p>2019.11.09 　studioちょっと更新</p>';
     // html += '<p>2019.11.05 　js5&jqueryちょっと更新</p>';
     // html += '<p>2019.11.03 　htm&css、js5&jquery更新</p>';
